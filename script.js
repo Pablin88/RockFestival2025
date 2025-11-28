2207ce6423b6d528ab273c12aeba87966c07d1c0
Pablin88 <pablo.zalazar@davinci.edu.ar>
2025-11-27 23:09:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+document.querySelectorAll('.faq-question').forEach(btn => {
+  btn.addEventListener('click', () => {
+    const item = btn.parentElement;
+
+    document.querySelectorAll('.faq-item').forEach(other => {
+      if (other !== item) {
+        other.classList.remove('active');
+      }
+    });
+
+    item.classList.toggle('active');
+  });
+});
+
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+    e.preventDefault(); // evita el envío real
+
+    // Ocultar formulario
+    document.getElementById('formularioContacto').style.display = 'none';
+
+    // Mostrar mensaje
+    document.getElementById('mensajeGracias').style.display = 'block';
+
+    // Reaparecer formulario en 5 segundos
+    setTimeout(() => {
+        document.getElementById('mensajeGracias').style.display = 'none';
+        document.getElementById('formularioContacto').style.display = 'block';
+        document.getElementById('contactForm').reset(); // limpiar campos
+    }, 3000);
+});
