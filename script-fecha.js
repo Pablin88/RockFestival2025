b01e8b260054ec247207317deb3ecb36a52e7a51
Pablin88 <pablo.zalazar@davinci.edu.ar>
2025-11-27 21:27:59 -0300

    Delete script-fecha.js

--- script-fecha.js
+++
@@ -1,6 +0,0 @@
-  // Get current date and time
-  var now = new Date();
-  var datetime = now.toLocaleString();
-
-  // Insert date and time into HTML
-  document.getElementById("datetime").innerHTML = datetime;
